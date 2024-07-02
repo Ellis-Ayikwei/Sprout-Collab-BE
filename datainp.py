9b4ad011a217a9916f6406fd460689a1327eff76
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-07-02 01:54:35 +0000

    the merge

--- datainp.py
+++ datainp.py
@@ -12,7 +12,7 @@ from models.collaboration import Collaboration
 from models.collaboration_member import Collaboration_member
 from models.project_member  import Project_member
 
-storage.close()
+#storage.close()
 storage.reload()
 num_goals = 10
 
