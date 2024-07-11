fbc44e55e6423ad218f901cdccbec831b8ea6bb6
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-07-11 00:59:58 +0000

    "Added new endpoint /project/myprojects/<user_id> to retrieve projects a user is a member of"

--- api/v1/views/project_member.py
+++ api/v1/views/project_member.py
@@ -10,6 +10,26 @@ from flasgger.utils import swag_from
 
 
 
+@app_views.route('/project/myprojects/<user_id>', methods=['GET'], strict_slashes=False)
+def get_my_projects(user_id):
+    """
+    Retrieves the list of projects that a user is a member of.
+    """
+    projects = storage.all(Project).values()
+    project_members = storage.all(Project_member).values()
+
+    my_projects = [project for project_member in project_members
+                   if project_member.user_id == user_id
+                   for project in projects
+                   if project.id == project_member.project_id]
+    
+    the_projs_user_is_part_of = [filter(lambda p_m: p_m.user_id == user_id, project_members).to_dict()]
+
+    project_dicts = [project.to_dict() for project in my_projects]
+
+    return jsonify({project_dicts,  the_projs_user_is_part_of})
+
+
 @app_views.route('/project_members', methods=['GET'], strict_slashes=False)
 def get_all_project_members():
     """
