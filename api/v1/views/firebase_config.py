75ae8594ab3a87d0ca5886fc475e9246e4ffa88c
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-10-24 21:12:29 +0000

    Update firebase config file path

--- api/v1/views/firebase_config.py
+++ api/v1/views/firebase_config.py
@@ -2,7 +2,7 @@ import firebase
 from firebase_admin import credentials
 import firebase_admin
 
-cred = credentials.Certificate("./sprout-collab-65cc2-firebase-adminsdk-5r6h4-8cce56a4a7.json")
+cred = credentials.Certificate("../sprout-collab-65cc2-firebase-adminsdk-5r6h4-8cce56a4a7.json")
 firebase_admin.initialize_app(cred, {
     'storageBucket' : 'sprout-collab-65cc2.appspot.com'
 })
