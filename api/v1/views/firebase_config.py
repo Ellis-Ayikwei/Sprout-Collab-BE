87e82cc6a2b7e8d64395adc972246b72f81fa3c0
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-10-27 18:42:17 +0000

    Refactor firebase_config.py to update firebase credentials path

--- api/v1/views/firebase_config.py
+++ api/v1/views/firebase_config.py
@@ -2,8 +2,8 @@ import firebase
 from firebase_admin import credentials
 import firebase_admin
 
-# cred = credentials.Certificate("/home/ellis_1/SC_WEB_APP/sc_cred_firebase.json")
-cred = credentials.Certificate('C:/Users/Ellis Rockefeller/Downloads/sprout-collab-65cc2-firebase-adminsdk-5r6h4-793065245b.json')
+cred = credentials.Certificate("/home/ellis_1/SC_WEB_APP/sc_cred_firebase.json")
+# cred = credentials.Certificate('C:/Users/Ellis Rockefeller/Downloads/sprout-collab-65cc2-firebase-adminsdk-5r6h4-793065245b.json')
 firebase_admin.initialize_app(cred, {
     'storageBucket': 'sprout-collab-65cc2.appspot.com'
 })
