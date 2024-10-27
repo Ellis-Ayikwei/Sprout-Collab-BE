c955dc6e29940851afea1503b5e80039fb5076f5
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-10-27 18:50:30 +0000

    Refactor CORS configuration in app.py to enable specific origins and use create_app function

--- api/v1/app.py
+++ api/v1/app.py
@@ -120,28 +120,28 @@ def create_app():
     app.register_blueprint(app_auth)
 
 
-    # @app.before_request
-    # def handle_preflight():
-    #     if request.method == 'OPTIONS':
-    #         response = make_response()
-    #         response.headers["Access-Control-Allow-Origin"] = request.headers.get("Origin", "*")
-    #         response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization, X-Refresh-Token"
-    #         response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
-    #         response.headers["Access-Control-Allow-Credentials"] = "true"
-    #         return response, 204
+    @app.before_request
+    def handle_preflight():
+        if request.method == 'OPTIONS':
+            response = make_response()
+            response.headers["Access-Control-Allow-Origin"] = request.headers.get("Origin", "*")
+            response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization, X-Refresh-Token"
+            response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+            response.headers["Access-Control-Allow-Credentials"] = "true"
+            return response, 204
         
         
     
 
-    # @app.after_request
-    # def after_request(response):
-    #     origin = request.headers.get("Origin")
-    #     if origin in allowed_origins:
-    #         response.headers["Access-Control-Allow-Origin"] = origin
-    #     response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization, X-Refresh-Token"
-    #     response.headers["Access-Control-Expose-Headers"] = "Authorization, X-Refresh-Token"
-    #     response.headers["Access-Control-Allow-Credentials"] = "true"
-    #     return response
+    @app.after_request
+    def after_request(response):
+        origin = request.headers.get("Origin")
+        if origin in allowed_origins:
+            response.headers["Access-Control-Allow-Origin"] = origin
+        response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization, X-Refresh-Token"
+        response.headers["Access-Control-Expose-Headers"] = "Authorization, X-Refresh-Token"
+        response.headers["Access-Control-Allow-Credentials"] = "true"
+        return response
         
     
     # Configure Swagger
