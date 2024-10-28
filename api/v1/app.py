151cec553022d4e273a94a174114b32ab528fe30
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-10-28 01:58:26 +0000

    Refactor app.py to use prefixed environment variables for mail configuration

--- api/v1/app.py
+++ api/v1/app.py
@@ -82,8 +82,8 @@ def create_app():
     app.config['MAIL_USE_TLS'] = True
     app.config.from_prefixed_env()
     
-    app.config['MAIL_USERNAME']
-    app.config['MAIL_PASSWORD']
+    app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+    app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 
     
     @jwt.token_in_blocklist_loader
