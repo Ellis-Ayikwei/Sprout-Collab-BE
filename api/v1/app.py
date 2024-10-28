4cfd741fb073369829e237449bc746e6ea6b86b4
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-10-28 02:06:37 +0000

    Refactor app.py to initialize mail app after configuring mail settings

--- api/v1/app.py
+++ api/v1/app.py
@@ -76,7 +76,6 @@ ACCESS_EXPIRES = datetime.timedelta(hours=1)
 def create_app():
     app = Flask(__name__)
     jwt =JWTManager(app)
-    mail.init_app(app)
     
     app.config['MAIL_SERVER'] = 'smtp.gmail.com'
     app.config['MAIL_PORT'] = 587
@@ -86,6 +85,7 @@ def create_app():
     app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
     app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 
+    mail.init_app(app)
     
     @jwt.token_in_blocklist_loader
     def check_if_token_is_revoked(jwt_header, jwt_payload: dict):
